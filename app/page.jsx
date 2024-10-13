f4ce8e06fa80ad6c89cb820e3f782ce7a9f54480
Priyanshu Gupta <priyanshug712.connect@gmail.com>
2024-10-13 15:52:35 +0530

    resume added

--- app/page.jsx
+++ app/page.jsx
@@ -21,10 +21,12 @@ const Home = () => {
               I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl " />
-              </Button>
+              <a href="/resume.pdf" download>
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download Resume</span>
+                  <FiDownload className="text-xl " />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
               />
